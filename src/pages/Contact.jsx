b6aa0c59d1c306bede3fa991fd45887481de007b
yahm0 <stnt190@outlook.com>
2024-05-20 20:12:36 -0400

    added logic to the Contact  page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm';
+import '../assets/styles/Contact.css';
+
+function Contact() {
+  return (
+    <section className="contact">
+      <h2>Contact Me</h2>
+      <ContactForm />
+    </section>
+  );
+}
+
+export default Contact;
