b5e5f8edd72c9d093b55f424dcc509c1ac8ba33f
yahm0 <stnt190@outlook.com>
2024-05-20 20:17:05 -0400

    added comments to the to the portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Project from '../components/Project';
 import '../assets/styles/Portfolio.css';
 
+// Array of project objects to be displayed in the portfolio
 const projects = [
   {
     title: 'Project 1',
@@ -21,13 +22,14 @@ const projects = [
 function Portfolio() {
   return (
     <section className="portfolio">
+      {/* Map over the projects array to render a Project component for each project */}
       {projects.map((project, index) => (
         <Project
-          key={index}
-          title={project.title}
-          image={project.image}
-          deployedLink={project.deployedLink}
-          repoLink={project.repoLink}
+          key={index} // Unique key for each Project component
+          title={project.title} // Project title
+          image={project.image} // Project image
+          deployedLink={project.deployedLink} // Link to deployed project
+          repoLink={project.repoLink} // Link to project's GitHub repository
         />
       ))}
     </section>
