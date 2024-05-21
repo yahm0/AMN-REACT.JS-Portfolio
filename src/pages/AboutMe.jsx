bb334b5f990c829c92d15e3f1675455a5f3db755
yahm0 <stnt190@outlook.com>
2024-05-20 20:10:59 -0400

    added logic to the AboutMe page.

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,9 +4,9 @@ import '../assets/styles/AboutMe.css';
 function AboutMe() {
   return (
     <section className="about-me">
-      <img src="/path-to-your-avatar.jpg" alt="Developer" />
+      <img src="../public/Screenshot 2023-12-11 1424311111.png" alt="Developer" />
       <h2>About Me</h2>
-      <p>This is a short bio about the developer.</p>
+      <p>As an IT Director, I am passionate about mentoring and exceptional leadership. With a commitment to fostering growth and innovation, I strive to inspire and guide teams towards achieving their fullest potential.</p>
     </section>
   );
 }
