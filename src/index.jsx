8091f2ac206fced90d8a6d44fc1ff72c7abc2ead
yahm0 <stnt190@outlook.com>
2024-05-20 20:19:29 -0400

    added comments to the to the to the to the index.jsx and the reportWebVitals.js

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,13 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Wrap the App component in React.StrictMode to highlight potential problems
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // Mount the App component to the DOM element with the id 'root'
 );
 
+// Call reportWebVitals to measure performance of the app
 reportWebVitals();
