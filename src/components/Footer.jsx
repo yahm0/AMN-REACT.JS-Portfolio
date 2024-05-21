1446827bd1b43cda70439e4966fcfdbe4b6f755c
yahm0 <stnt190@outlook.com>
2024-05-20 20:04:51 -0400

    added logic to the Footer.jsx file that will generate components and have links to my other pages.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../assets/styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Adam Nuccio</p>
+      <div>
+        <a href="https://github.com/yahm0">GitHub</a>
+        <a href="https://www.linkedin.com/in/adam-nuccio/">LinkedIn</a>
+        <a href="https://stackoverflow.com/users/25155278/yahmo">Stack Overflow</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
