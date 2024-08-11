ab2a2eee1e4167940036e43fcd6eec030cb47a4d
yahm0 <stnt190@outlook.com>
2024-08-11 17:57:44 -0400

    App.jsx rolled back.

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,29 @@ import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import './styles/App.css';
 
+// Main App component
 function App() {
   return (
+    // Router to manage application routing
     <Router>
-      <div className="background-container">
-        <img src="/assets/imgs/background.jpg" alt="Background" className="background-image" />
-        <div className="App">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<AboutMe />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/resume" element={<Resume />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+      <div className="App">
+        {/* Header component to be displayed on all pages */}
+        <Header />
+        <main>
+          {/* Routes component to define the routes of the application */}
+          <Routes>
+            {/* Route for the About Me page */}
+            <Route path="/" element={<AboutMe />} />
+            {/* Route for the Portfolio page */}
+            <Route path="/portfolio" element={<Portfolio />} />
+            {/* Route for the Contact page */}
+            <Route path="/contact" element={<Contact />} />
+            {/* Route for the Resume page */}
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </main>
+        {/* Footer component to be displayed on all pages */}
+        <Footer />
       </div>
     </Router>
   );
