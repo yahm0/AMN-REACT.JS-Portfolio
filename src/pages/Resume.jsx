337792a770f5d7ccb6598cda19926cf5b08b493f
yahm0 <stnt190@outlook.com>
2024-08-11 13:39:50 -0400

    Made changes to the Resume Page.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,6 +5,14 @@ function Resume() {
   return (
     <section className="resume">
       <h2>Resume</h2>
+      <div className="pdf-viewer">
+        <iframe
+          src="assets/resume/ANResume2024_combined.pdf"
+          width="100%"
+          height="500px"
+          title="Resume PDF"
+        ></iframe>
+      </div>
       <a href="assets/resume/ANResume2024_combined.pdf" download className="resume-download">
         Download Resume
       </a>
