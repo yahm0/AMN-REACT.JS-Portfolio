3a88830fe11c4e6fb434abdefa39b774ceb24521
yahm0 <stnt190@outlook.com>
2024-08-11 11:12:25 -0400

    Made changes to the resume.jsx.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,9 +6,8 @@ function Resume() {
     <section className="resume">
       <h2>Resume</h2>
       <a href="assets/resume/ANResume2024_combined.pdf" download>Download Resume</a>
-      {/* Add more proficiencies as needed */}
-    </ul>
-    </section >
+      {/* Add more content as needed */}
+    </section>
   );
 }
 
