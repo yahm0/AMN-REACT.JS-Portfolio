e315ae03c419c85449e68659ca5fe9a5ed60f5fd
yahm0 <stnt190@outlook.com>
2024-08-11 17:25:04 -0400

    added responsive design.

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,24 @@ import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import './styles/App.css';
 
+// Main App component
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* Use a div for general-purpose background container */}
         <div className="background-container">
-          <img src="/assets/imgs/overlapping-white-grey-gradient-circles-background-vector.jpg" alt="Background" className="background-image" />
+          <img src={`${process.env.PUBLIC_URL}/assets/imgs/overlapping-white-grey-gradient-circles-background-vector.jpg`} alt="Background" className="background-image" />
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<AboutMe />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/resume" element={<Resume />} />
+            </Routes>
+          </main>
+          <Footer />
         </div>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<AboutMe />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-        </main>
-        <Footer />
       </div>
     </Router>
   );
