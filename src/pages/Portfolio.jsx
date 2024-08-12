765cb779325295331c28f80c28a1fcf5136c83e0
yahm0 <stnt190@outlook.com>
2024-08-12 10:56:07 -0400

    changed the indexing of which project shows on the page.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,12 +5,6 @@ import '../styles/Portfolio.css';
 // Array of project objects to be displayed in the portfolio
 const projects = [
   {
-    title: 'Book Search Engine and Manager',
-    image: '/assets/imgs/books.jpg',
-    deployedLink: 'https://book-search-engine-and-manager-16b16f7fd358.herokuapp.com/',
-    repoLink: 'https://github.com/yahm0/Book-Search-Engine-And-Manager'
-  },
-  {
     title: 'Green Pipe Analytics Website',
     image: '',
     deployedLink: 'https://greenpipeanalytics.com',
@@ -27,6 +21,12 @@ const projects = [
     image: '',
     deployedLink: 'https://junk-twitter-1f692e920bc6.herokuapp.com/login',
     repoLink: 'https://github.com/yahm0/Junk-Twitter'
+  },
+  {
+    title: 'Book Search Engine and Manager',
+    image: '/assets/imgs/books.jpg',
+    deployedLink: 'https://book-search-engine-and-manager-16b16f7fd358.herokuapp.com/',
+    repoLink: 'https://github.com/yahm0/Book-Search-Engine-And-Manager'
   },
   {
     title: 'Eventii Management Platform',
